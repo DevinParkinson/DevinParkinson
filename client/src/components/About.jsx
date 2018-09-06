fc165469d3e41ad98577dd5a19a334963fe2cc8f
Devin Parkinson <dparky13@icloud.com>
2018-09-06 14:18:46 -0600

    updating nav bar and about page

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -4,7 +4,11 @@ import { Header } from 'semantic-ui-react'
 class About extends Component {
   render() {
     return (
-      <Header>Wanna know a little bit more about me?</Header>
+      <div>
+        <Header>Wanna know a little bit more about me?</Header>
+        <div>My Family</div>
+        <div>My Hobbies</div>
+      </div>
     )
   }
 
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -9,23 +9,16 @@ class NavBar extends Component {
     return (
       <div>
         <Menu pointing secondary>
-        <Dropdown icon="bars" floating button className='icon'>
-          <Dropdown.Menu>
-            <Dropdown.Item>
+          <Menu.Item>
               <Link to='/' style={{color: "black"}}>
-                <Icon name="home" size="large" />
                 Home
               </Link>
-            </Dropdown.Item>
-            <Divider />
-            <Dropdown.Item>
+            </Menu.Item>
+            <Menu.Item>
               <Link to="/about" style={{color: "black"}}>
-                <Icon name="question circle outline" size="large" />
                 Personal Life
               </Link>
-            </Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
+            </Menu.Item>
         <Menu.Item position="right">
           <Header as="h3" style={{fontFamily: 'coven-medium,sans-serif'}}>Devin Parkinson</Header>
         </Menu.Item>
